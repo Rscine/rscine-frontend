5a67311053396679e16ea18a939d30f2b2caa827
monzey <maxi.bertrand@gmail.com>
2016-10-12 23:50:22 +0200

    created new route (profile)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -32,6 +32,11 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl',
+        controllerAs: 'profile'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/profile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rscineFrontendApp.controller:ProfileCtrl
+ * @description
+ * # ProfileCtrl
+ * Controller of the rscineFrontendApp
+ */
+angular.module('rscineFrontendApp')
+    .controller('ProfileCtrl', function ($scope) {
+
+    });
