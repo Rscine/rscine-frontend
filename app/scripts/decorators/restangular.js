800b8bcc7ab643b8d5c2264272cb3a53d9b513cd
Maxime Bertrand <maxi.bertrand@gmail.com>
2016-10-14 13:48:32 +0200

    fixed acces_token shipment for getList method

--- app/scripts/decorators/restangular.js
+++ app/scripts/decorators/restangular.js
@@ -9,7 +9,7 @@
  */
 angular.module('rscineFrontendApp')
     .config(function ($provide) {
-        $provide.decorator('Restangular', function ($delegate, oAuthAuthentication) {
+        $provide.decorator('Restangular', function ($delegate, oAuthAuthentication, $http) {
             $delegate.getBaseUrl = function () {
                 return 'http://api.rscine.dev';
             }
@@ -17,7 +17,7 @@ angular.module('rscineFrontendApp')
             $delegate.setBaseUrl($delegate.getBaseUrl() + '/api/v1');
 
             $delegate.addFullRequestInterceptor(function (element, operation, what, url, headers, params, httpConfig) {
-                if (operation == 'get') {
+                if (operation == 'get' || operation =='getList') {
                     params['access_token'] = oAuthAuthentication.getToken();
 
                     return {
@@ -31,7 +31,7 @@ angular.module('rscineFrontendApp')
 
             $delegate.setErrorInterceptor(function(response, deferred, responseHandler) {
                 if(response.status === 401) {
-                    oAuthAuthentication.getAccessTokenByRefreshToken(oAuthAuthentication.getToken()).then(function() {
+                    oAuthAuthentication.getAccessTokenByRefreshToken(oAuthAuthentication.getRefreshToken()).then(function() {
                         // Repeat the request and then call the handlers the usual way.
                         $http(response.config).then(responseHandler, deferred.reject);
                         // Be aware that no request interceptors are called this way.
