93fe7cb7e5050f6064614cd23beeeb96d95252a6
Maxime Bertrand <maxi.bertrand@gmail.com>
2016-10-14 14:02:26 +0200

    changed way of getting companies from profile page (now using id link from api)

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -11,7 +11,7 @@ angular.module('rscineFrontendApp')
     .controller('ProfileCtrl', function ($scope, Restangular) {
         Restangular.one('users', 'me').get().then(function (user) {
             $scope.user = user;
-            Restangular.oneUrl('companies', Restangular.getBaseUrl() + user._links.company.href).get().then(function (company) {
+            Restangular.one('companies', user._links.company.id).get().then(function (company) {
                 $scope.user.company = company;
             })
         })
